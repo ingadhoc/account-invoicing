45e75177d13e57531ee550a150a74e51c8a45086
Juan Jose Scarafia <jjs@adhoc.com.ar>
2016-03-16 17:46:40 -0300

    IMP invoice move currency module

--- account_invoice_move_currency/models/account_invoice.py
+++ account_invoice_move_currency/models/account_invoice.py
@@ -12,9 +12,30 @@ class AccountInvoice(models.Model):
 
     move_currency_id = fields.Many2one(
         'res.currency',
-        'Move Currency',
-        help='Account Move Secondary Currency'
+        'Account Move Secondary Currency',
+        help='If you set a currency here, then this invoice values will be '
+        'also stored in the related Account Move',
+        copy=False,
+        readonly=True,
+        states={'draft': [('readonly', False)]},
         )
+    # TODO implement this
+    # move_currency_rate = fields.Float(
+    move_inverse_currency_rate = fields.Float(
+        copy=False,
+        digits=(16, 4),
+        string='Account Move Secondary Currency Rate',
+        readonly=True,
+        states={'draft': [('readonly', False)]},
+        )
+
+    @api.onchange('move_currency_id')
+    def change_move_currency(self):
+        if not self.move_currency_id:
+            self.move_inverse_currency_rate = False
+        self.move_inverse_currency_rate = self.env[
+            'res.currency']._get_conversion_rate(
+            self.move_currency_id, self.company_id.currency_id)
 
     @api.constrains('move_currency_id', 'currency_id')
     def check_move_currency(self):
@@ -37,17 +58,23 @@ class AccountInvoice(models.Model):
         move_lines = super(
             AccountInvoice, self).finalize_invoice_move_lines(move_lines)
         if self.move_currency_id:
-            company_currency = self.company_id.currency_id
+            # company_currency = self.company_id.currency_id
             for a, b, line in move_lines:
-                move_currency = self.move_currency_id.with_context(
-                    date=self.date_invoice or fields.Date.context_today(self))
+                # move_currency = self.move_currency_id.with_context(
+                #     date=self.date_invoice or fields.Date.context_today(
+                #     self))
+                if not self.move_inverse_currency_rate:
+                    raise Warning(_(
+                        'If Move Secondary Currency Select you must set rate'))
                 if line['debit']:
                     amount = line['debit']
                     sign = 1.0
                 else:
                     amount = line['credit']
                     sign = -1.0
-                line['currency_id'] = move_currency.id
-                line['amount_currency'] = sign * company_currency.compute(
-                    amount, move_currency)
+                line['currency_id'] = self.move_currency_id.id
+                line['amount_currency'] = sign * self.move_currency_id.round(
+                    amount / self.move_inverse_currency_rate)
+                # line['amount_currency'] = sign * company_currency.compute(
+                #     amount, move_currency)
         return move_lines
