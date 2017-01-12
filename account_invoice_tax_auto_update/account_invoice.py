d98c3867a8f36cf2e98100ec6cc4f292a98faf21
Juan Jose Scarafia <jjs@adhoc.com.ar>
2017-01-12 08:48:39 -0300

    FIX

--- account_invoice_tax_auto_update/account_invoice.py
+++ account_invoice_tax_auto_update/account_invoice.py
@@ -10,6 +10,11 @@ class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
     @api.multi
+    def button_reset_taxes(self):
+        return super(AccountInvoice, self.with_context(
+            constraint_update_taxes=True)).button_reset_taxes()
+
+    @api.multi
     @api.constrains('invoice_line')
     def update_taxes(self):
         context = dict(self._context)
