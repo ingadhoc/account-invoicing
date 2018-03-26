d617f95a32b0aec629a4fd72adcc010185589c58
Nicolas Mac Rouillon <nicomacr@users.noreply.github.com>
2018-03-26 19:28:25 -0300

    [REM] delete account_invoice_operation, deprecated for v11 (#45)

--- account_invoice_operation/models/__init__.py
+++
@@ -1,8 +0,0 @@
-##############################################################################
-# For copyright and license notices, see __manifest__.py file in module root
-# directory
-##############################################################################
-from . import account_invoice_plan
-from . import account_invoice_operation
-from . import account_invoice
-from . import res_partner
