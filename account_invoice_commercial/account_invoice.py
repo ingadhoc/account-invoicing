b476383794dc6225e0be81b87bdbd604f09aabc1
Katherine Zaoral <kz@adhoc.com.ar>
2018-08-07 14:03:51 -0300

    [FIX] account_invoice_commercial: Only overwrite the user_id in the invoice with the partner commercial if this one is set. if not will set the default value.

--- account_invoice_commercial/account_invoice.py
+++ account_invoice_commercial/account_invoice.py
@@ -5,11 +5,7 @@ class account_invoice(models.Model):
 
     _inherit = 'account.invoice'
 
-    @api.onchange('partner_id', 'company_id')
-    def _onchange_partner_id(self):
-        ret = super(account_invoice, self)._onchange_partner_id()
-        if self.partner_id and self.partner_id.user_id:
+    @api.onchange('partner_id')
+    def _onchange_partner_commercial(self):
+        if self.partner_id.user_id:
             self.user_id = self.partner_id.user_id.id
-        else:
-            self.user_id = self.env.uid
-        return ret
