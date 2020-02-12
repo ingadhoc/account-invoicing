00b5ab0b960c5b76c64c051689b7c6fee26cae34
Daniel Libonati <dl@adhoc.com.ar>
2020-02-12 11:40:50 -0300

    [MIG] account_invoice_line_number: Migration to 13.0

--- account_invoice_line_number/__manifest__.py
+++ account_invoice_line_number/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Account Invoice Line Number',
-    'version': '12.0.1.0.0',
+    'version': "13.0.1.0.0",
     'category': 'Accounting & Finance',
     'sequence': 14,
     'author': 'ADHOC SA',
@@ -37,7 +37,7 @@
     ],
     'test': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
