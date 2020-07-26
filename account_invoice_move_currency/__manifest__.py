d305bcd674487e94ba9c7591252f6fdb681a2879
Juan Jose Scarafia <jjs@adhoc.com.ar>
2020-07-25 21:15:26 -0300

    [IMP] move_currency: show only on invoices

--- account_invoice_move_currency/__manifest__.py
+++ account_invoice_move_currency/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Account Invoice Move Currency',
-    'version': '13.0.1.0.0',
+    'version': '13.0.1.1.0',
     'author': 'ADHOC SA',
     'category': 'Accounting & Finance',
     'depends': [
