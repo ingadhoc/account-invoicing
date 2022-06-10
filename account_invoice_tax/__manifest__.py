289ea0509d0a03aeca1fa1dd5271d4883ecb25f7
Juan Jose Scarafia <jjs@adhoc.com.ar>
2022-06-10 13:12:03 +0000

    [MIG] account_invoice_tax: Migration to 15.0
    closes ingadhoc/account-invoicing#136
    
    Signed-off-by: Juan José Scarafía <jjs@adhoc.com.ar>

--- account_invoice_tax/__manifest__.py
+++ account_invoice_tax/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Account Invoice Tax',
-    'version': "13.0.1.1.0",
+    'version': "15.0.1.0.0",
     'author': 'ADHOC SA',
     'category': 'Localization',
     'depends': [
@@ -10,9 +10,10 @@
     'data': [
         'wizards/account_invoice_tax_view.xml',
         'views/account_move_view.xml',
+        'security/ir.model.access.csv',
     ],
     'license': 'AGPL-3',
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
