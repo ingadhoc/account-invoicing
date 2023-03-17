21fdb3f2b7017e3a1b15ed698261df2f995805e4
docker-odoo <awe@adhoc.com.ar>
2023-03-17 19:17:50 +0000

    [MIG] account_invoice_control: Migration to 16.0

--- account_invoice_control/__manifest__.py
+++ account_invoice_control/__manifest__.py
@@ -20,7 +20,7 @@
 {
     'name': 'Account Invoice Control',
     'author': 'ADHOC SA',
-    'version': "15.0.1.0.0",
+    'version': "16.0.1.0.0",
     'license': 'AGPL-3',
     'category': 'Accounting & Finance',
     'depends': [
@@ -31,5 +31,5 @@
         'views/account_move_views.xml',
     ],
     'website': 'www.adhoc.com.ar',
-    'installable': False,
+    'installable': True,
 }
