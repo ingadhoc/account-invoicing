3784498365c5af4fa369e609eb6773306f7955cf
Juan José Scarafia <jjs@adhoc.com.ar>
2023-09-12 22:14:14 -0300

    [REF] _move_currency: simplified Depreciamos todo lo que impactaba en el apunte contable porque era la parte mas heavy y dejamos solo a modo informativo una moneda secundaria y su cotizacion

--- account_invoice_move_currency/wizards/account_change_currency.py
+++ account_invoice_move_currency/wizards/account_change_currency.py
@@ -9,13 +9,8 @@ class AccountChangeCurrency(models.TransientModel):
     _inherit = 'account.change.currency'
 
     save_secondary_currency = fields.Boolean('Save in secondary currency?')
-
     same_currency = fields.Boolean(compute='_compute_same_currency')
-
-    currency_company_id = fields.Many2one(
-        'res.currency',
-        related='move_id.company_id.currency_id'
-    )
+    currency_company_id = fields.Many2one('res.currency', related='move_id.company_id.currency_id')
 
     @api.depends('currency_company_id', 'currency_to_id')
     def _compute_same_currency(self):
@@ -33,8 +28,7 @@ class AccountChangeCurrency(models.TransientModel):
             self.move_id.move_inverse_currency_rate = False
         currency_from_id = self.currency_from_id
         res = super().change_currency()
-        if self.save_secondary_currency and self.same_currency and \
-                self.change_type == 'value':
+        if self.save_secondary_currency and self.same_currency and self.change_type == 'value':
             self.move_id.move_currency_id = currency_from_id.id
             self.move_id.move_inverse_currency_rate = self.currency_rate
         return res
