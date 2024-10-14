113740f9ed355bc7e179f786cdb467eb9730cee0
docker-odoo <docker-odoo@example.com>
2024-10-14 17:02:30 +0000

    [MIG] account_invoice_prices_update: Migration to 18.0

--- account_invoice_prices_update/__manifest__.py
+++ account_invoice_prices_update/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Account Invoice Prices Update',
-    'version': "17.0.1.0.0",
+    'version': "18.0.1.0.0",
     'author': 'ADHOC SA',
     'license': 'AGPL-3',
     'category': 'Accounting & Finance',
@@ -32,5 +32,5 @@
         'views/account_move_views.xml',
         'security/ir.model.access.csv',
     ],
-    'installable': False,
+    'installable': True,
 }
