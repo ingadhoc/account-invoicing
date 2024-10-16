58e6673a79cc7bb5ec8714528f98f0a5a0c553b5
docker-odoo <docker-odoo@example.com>
2024-10-16 18:23:30 +0000

    [MIG] account_invoice_commission: Migration to 18.0

--- account_invoice_commission/__manifest__.py
+++ account_invoice_commission/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     'name': 'Commission Invoices',
-    'version': "17.0.1.0.0",
+    'version': "18.0.1.0.0",
     'category': 'Accounting',
     'sequence': 14,
     'summary': '',
@@ -40,7 +40,7 @@
     ],
     'test': [
     ],
-    'installable': False,
+    'installable': True,
     'auto_install': False,
     'application': False,
 }
