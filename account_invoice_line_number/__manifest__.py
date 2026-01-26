cc714def25dd757e8335b334e1c6a5c2b095fde5
migration-bot-adhoc <migration-bot-adhoc@users.noreply.github.com>
2026-01-26 13:22:47 +0000

    [MIG] account_invoice_line_number: Migration to 18.0

--- account_invoice_line_number/__manifest__.py
+++ account_invoice_line_number/__manifest__.py
@@ -19,7 +19,7 @@
 ##############################################################################
 {
     "name": "Account Invoice Line Number",
-    "version": "15.0.1.1.0",
+    "version": "18.0.1.0.0",
     "category": "Accounting & Finance",
     "sequence": 14,
     "author": "ADHOC SA",
@@ -34,7 +34,7 @@
     ],
     "demo": [],
     "test": [],
-    "installable": False,
+    "installable": True,
     "auto_install": False,
     "application": False,
 }
