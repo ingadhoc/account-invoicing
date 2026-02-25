23896eefc0589a398b95bdb36b92a3dc2812b91e
“Victoria <hav@adhoc.inc>
2026-02-25 15:05:09 +0000

    [ADD] l10n_latam_invoice_document_ux: new module to add UX improvements to the invoice document in latam countries

---
+++ l10n_latam_invoice_document_ux/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Latam Invoice Document UX",
+    "version": "19.0.1.0.0",
+    "author": "ADHOC SA",
+    "website": "www.adhoc.com.ar",
+    "license": "AGPL-3",
+    "category": "Localization",
+    "depends": [
+        "l10n_latam_invoice_document",
+    ],
+    "data": [
+        "views/account_move_view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
