c50ef7d5f9844cc085d3c6bc8b4bf63c9d47b1c7
Camila Vives <cav@adhoc.com.ar>
2026-03-27 13:56:36 +0000

    [FIX] account_background_post: commit after posting a message on error
    When validating an invoice on the background, if an error occurs, a message is posted.
    However, since the transaction is not committed yet, the message is sent with a delay and only after the transaction is committed.
    This can be problematic if the error is due to a validation error, as the transaction will be rolled back and the message will never be sent.
    
    X-original-commit: ca061fad3b3ff8e38845ba012c07042ffc9602d2

--- account_background_post/models/account_move.py
+++ account_background_post/models/account_move.py
@@ -44,9 +44,28 @@ class AccountMove(models.Model):
                     partner_ids=move.get_internal_partners().ids,
                     body_is_html=True,
                 )
+<<<<<<< 24dad28271a7be0f910c94eae5e492a680c86b6a
                 _logger.error("Error while trying to post invoice %s in background: %s", move.name, exp)
                 # Commit after each failure to set false background_post and post the message
                 self.env.cr.commit()  # pylint: disable=invalid-commit
+||||||| e2e735e96b98dcfb95dc414b45e83a0e358e6ebc
+        if len(moves) > batch_size:
+            cron_id = self.env.context.get("cron_id")
+            if cron_id:
+                # Si tenemos cron_id en el contexto, usamos ese para relanzar la ejecucion.
+                self.env["ir.cron"].browse(self.env.context["cron_id"])._trigger()
+                return
+            self.env.ref("account_background_post.ir_cron_background_post_invoices")._trigger()
+=======
+                self.env.cr.commit()  # pragma pylint: disable=invalid-commit
+        if len(moves) > batch_size:
+            cron_id = self.env.context.get("cron_id")
+            if cron_id:
+                # Si tenemos cron_id en el contexto, usamos ese para relanzar la ejecucion.
+                self.env["ir.cron"].browse(self.env.context["cron_id"])._trigger()
+                return
+            self.env.ref("account_background_post.ir_cron_background_post_invoices")._trigger()
+>>>>>>> 845cecc44510119365244ebbd27b9d8ce1146047
 
     def _post(self, soft=True):
         posted = super()._post(soft=soft)
