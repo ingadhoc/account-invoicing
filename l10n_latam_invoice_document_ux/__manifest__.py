d4ac62113174727ad1a08fea1bef35543afafec0
“Victoria <hav@adhoc.inc>
2026-07-17 13:44:32 +0000

    [ADD] l10n_latam_invoice_document_ux: backport from 19.0
    Defer the document type/number requirement from the form to posting
    time, so a posted move can be reset to draft and have its number
    cleared (e.g. to change company/journal). Enforcement stays at post
    via the standard l10n_latam_invoice_document constraints. Also blocks
    changing the journal of a posted move that uses documents.
    
    X-original-commit: 0164141cb3238d9dfeca5d494725c535c95508e1

--- l10n_latam_invoice_document_ux/__manifest__.py
+++ l10n_latam_invoice_document_ux/__manifest__.py
@@ -1,3 +1,4 @@
+<<<<<<< 52e9856e64979832759bfb1af1f4b747aef4ff30
 {
     "name": "Latam Invoice Document UX",
     "version": "19.0.1.2.0",
@@ -15,3 +16,23 @@
     "auto_install": False,
     "application": False,
 }
+||||||| fdb4c1fade2d18f1c3a250cfd69a59c8310c6720
+=======
+{
+    "name": "Latam Invoice Document UX",
+    "version": "18.0.1.0.0",
+    "author": "ADHOC SA",
+    "website": "www.adhoc.com.ar",
+    "license": "AGPL-3",
+    "category": "Localization",
+    "depends": [
+        "l10n_latam_invoice_document",
+    ],
+    "data": [
+        "views/account_move_view.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+    "application": False,
+}
+>>>>>>> 05a4742d707ac637dd2c0994856a1588adaa7439
